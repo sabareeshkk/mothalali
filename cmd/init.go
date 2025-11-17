be90148b5b8af901e2fb312ffc06895701a2e5df
sabareeeshkk <sabareeshk1991@gmail.com>
2025-11-17 23:33:57 +0530

    .git directory cline added

--- cmd/init.go
+++ cmd/init.go
@@ -5,9 +5,13 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
+
+const GIT_DIR = ".mothalali"
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
@@ -17,7 +21,21 @@ var initCmd = &cobra.Command{
 Example:
   mothalali init`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called!!!")
+		absPath, err := filepath.Abs(GIT_DIR)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error getting absolute path: %v\n", err)
+			os.Exit(1)
+		}
+
+		if _, err := os.Stat(GIT_DIR); os.IsNotExist(err) {
+			if err := os.Mkdir(GIT_DIR, 0700); err != nil {
+				fmt.Fprintf(os.Stderr, "Error creating directory: %v\n", err)
+				os.Exit(1)
+			}
+			fmt.Printf("Initialized empty Mothalali repository in %s\n", absPath)
+		} else {
+			fmt.Printf("Mothalali repository already exists in %s\n", absPath)
+		}
 	},
 }
 
